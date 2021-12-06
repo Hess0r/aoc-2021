b1f2a0eb9073f1e9ec765b7f237a9cdb8c5ca2c0
Gábor Sinka <hessor@gmail.com>
2021-12-06 18:45:35 +0100

    Day 1

---
+++ day1/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("./input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	measurements := []int{}
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		int, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+		measurements = append(measurements, int)
+	}
+
+	var count int
+
+	for i, val := range measurements[1:] {
+		if val > measurements[i] {
+			count++
+		}
+	}
+
+	fmt.Println(count)
+}
